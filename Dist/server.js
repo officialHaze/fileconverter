0871c669204c9ce04dec88ceb1e0d7d818db854e
Assassin <moinakdey.166@outlook.com>
2023-08-13 17:15:02 +0530

    Added routes to server to handle callback more efficiently

--- Dist/server.js
+++ Dist/server.js
@@ -6,9 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const express_fileupload_1 = __importDefault(require("express-fileupload"));
-const FileConverter_1 = __importDefault(require("./Services/FileConverter"));
-const constants_1 = require("./Utils/constants");
-const fs_1 = __importDefault(require("fs"));
+const docToPdf_1 = __importDefault(require("./Routes/docToPdf"));
 const server = (0, express_1.default)();
 const PORT = 9000;
 const jsonParser = body_parser_1.default.json(); // To parse json in req
@@ -17,31 +15,4 @@ server.use((0, express_fileupload_1.default)());
 server.listen(PORT, "", () => {
     console.log(`Server is running on port ${PORT}`);
 });
-server.post("/convert/docToPdf", (req, res) => {
-    var _a;
-    const file = (_a = req.files) === null || _a === void 0 ? void 0 : _a.file;
-    console.log(file);
-    if (file) {
-        const fileConverter = new FileConverter_1.default(constants_1.PDF, file);
-        fileConverter.fromDocToPdf()
-            .then((outputPath) => {
-            res.download(outputPath, (err) => {
-                if (err) {
-                    console.error(err);
-                }
-                fs_1.default.unlink(outputPath, (err) => {
-                    if (err) {
-                        console.error(err);
-                    }
-                    else {
-                        console.log('File deleted');
-                    }
-                });
-            });
-        })
-            .catch(err => { res.status(500).json({ 'Error': err }); });
-    }
-    else {
-        res.status(400).json({ 'Error': 'No file to convert!' });
-    }
-});
+server.use("/convert/docToPdf", docToPdf_1.default);
--- server.ts
+++ server.ts
@@ -1,9 +1,7 @@
 import express from "express"
 import bodyParser from "body-parser"
 import fileUpload from "express-fileupload"
-import FileConverter from "./Services/FileConverter"
-import { PDF } from "./Utils/constants"
-import fs from "fs"
+import docToPdf from "./Routes/docToPdf"
 
 
 const server = express()
@@ -19,35 +17,4 @@ server.listen(PORT, "", ()=>{
     console.log(`Server is running on port ${PORT}`)
 })
 
-server.post("/convert/docToPdf", (req, res)=>{
-    const file = req.files?.file
-    console.log(file)
-    if(file)
-    {
-        const fileConverter = new FileConverter(PDF, file)
-        fileConverter.fromDocToPdf()
-        .then((outputPath)=>{
-            res.download(outputPath, (err)=>{
-                if(err)
-                {
-                    console.error(err)
-                }
-                fs.unlink(outputPath, (err)=>{
-                    if(err)
-                    {
-                        console.error(err)
-                    }
-                    else
-                    {
-                        console.log('File deleted')
-                    }
-                }) 
-            })
-        })
-        .catch(err=>{res.status(500).json({'Error': err})})
-    }
-    else
-    {
-        res.status(400).json({'Error': 'No file to convert!'})
-    }
-})
+server.use("/convert/docToPdf", docToPdf)
