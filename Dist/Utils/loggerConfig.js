8e37cdd75fea2188f2376beaf92c57c9050bcfd6
Assassin <moinak.dey8@gmail.com>
2023-10-01 21:04:04 +0530

    Updated the gitignore file

--- Dist/Utils/loggerConfig.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.devlogInstance = void 0;
-const logger_1 = require("@banglarthek/logger");
-exports.devlogInstance = logger_1.terminalLogger.create({
-    loggerEnvironments: ["development"],
-    environmentName: "DEVELOPMENT_ENVIRONMENT"
-});
